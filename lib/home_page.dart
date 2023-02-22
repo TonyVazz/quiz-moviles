47e591d3eb0aa3c61352daf6dbe68a8403b84eb4
TonyVazz <al19760723@ite.edu.mx>
2023-02-21 22:58:31 -0800

    termine la aplicacion quiz colocandole la alerta

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -20,16 +20,23 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int aciertos = 0;
   List<Icon> puntuacion = [];
   List<String> preguntas = [
     '¿Los glóbulos rojos viven 4 meses?',
     '¿El cuerpo humano adulto tiene 306 huesos?',
     '¿La cobalamina es una vitamina?',
+    '¿Los chochos hacen daño?',
+    '¿Voy a pasar la materia?',
+    'Es bueno hacer ejercicio 7 dias a la semana',
   ];
   List<bool> respuestas = [
     true,
     false,
     true,
+    true,
+    true,
+    false,
   ];
   int numeroPregunta = 0;
 
@@ -49,81 +56,59 @@ class _QuizPageState extends State<QuizPage> {
                 child: Center(
                     child: Text(
                   preguntas[numeroPregunta],
-                  style: const TextStyle(fontSize: 20),
+                  style: const TextStyle(
+                    fontSize: 38,
+                    color: Colors.white,
+                  ),
                 )),
               ),
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(17),
-                  ),
-                  child: TextButton(
-                    onPressed: (() {
-                      setState(() {
-                        bool respuestaCorrecta = respuestas[numeroPregunta];
-                        if (respuestaCorrecta == true) {
-                          print('la respuesta es correcta');
-                        } else {
-                          print('La respuesta es incorrecta');
-                        }
-
-                        numeroPregunta++;
-
-                      
-                      });
-                    }),
-                    child: const Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Center(
-                            child: Text(
-                          'Verdadero',
-                          style: TextStyle(color: Colors.white, fontSize: 30),
-                        )),
-                      ),
-                    ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(17),
+                ),
+                child: TextButton(
+                  onPressed: (() {
+                    setState(() {
+                      comprobar(true);
+                      //numeroPregunta++;
+                    });
+                  }),
+                  child: const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(
+                        child: Text(
+                      'Verdadero',
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    )),
                   ),
                 ),
               ),
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(17),
-                  ),
-                  child: TextButton(
-                    onPressed: (() {
-                      setState(() {
-                        bool respuestaCorrecta = respuestas[numeroPregunta];
-                        if (respuestaCorrecta == true) {
-                          print('la respuesta es correcta');
-                        } else {
-                          print('La respuesta es incorrecta');
-                        }
-                        numeroPregunta++;
-                        puntuacion.add(const Icon(
-                          Icons.close,
-                          color: Colors.red,
-                          size: 40,
-                        ));
-                      });
-                    }),
-                    child: const Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Center(
-                            child: Text('Falso',
-                                style: TextStyle(
-                                    color: Color.fromARGB(255, 27, 25, 25),
-                                    fontSize: 30))),
-                      ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(17),
+                ),
+                child: TextButton(
+                  onPressed: (() {
+                    setState(() {
+                      comprobar(false);
+                      //numeroPregunta++;
+                    });
+                  }),
+                  child: const Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                          child: Text('Falso',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 30))),
                     ),
                   ),
                 ),
@@ -138,8 +123,49 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 
-  void comprobar() {
+  void comprobar(comprobar) {
+    bool respuestaCorrecta = respuestas[numeroPregunta];
 
-    
+    if (respuestaCorrecta == comprobar) {
+      aciertos++;
+      puntuacion.add(const Icon(
+        Icons.check,
+        color: Colors.green,
+        size: 40,
+      ));
+    } else {
+      puntuacion.add(const Icon(
+        Icons.close,
+        color: Colors.red,
+        size: 40,
+      ));
+    }
+
+    if (respuestas.length - 1 != numeroPregunta) {
+      numeroPregunta++;
+    } else {
+      numeroPregunta = 0;
+      setState(() {
+        showDialog(context: context, builder: (_) => marcador());
+      });
+    }
+  }
+
+  marcador() {
+    return AlertDialog(
+      title:
+          Text("El marcador final es: $aciertos", textAlign: TextAlign.center),
+      content: ElevatedButton(
+        child: const Text("Reiniciar"),
+        onPressed: () {
+          Navigator.pop(context);
+          setState(() {
+            numeroPregunta = 0;
+            aciertos = 0;
+            puntuacion.clear();
+          });
+        },
+      ),
+    );
   }
 }
